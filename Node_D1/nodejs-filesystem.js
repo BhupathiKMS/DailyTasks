6fdc7d48dac58072c751fc8ea85edf893a637581
BhupathiKMS <kmsbhupathi@gmail.com>
2024-06-25 23:58:48 +0530

    port

--- Node_D1/nodejs-filesystem.js
+++ Node_D1/nodejs-filesystem.js
@@ -24,7 +24,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const port = 3000;
+const port = PROCESS.env.port || 3000;
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
